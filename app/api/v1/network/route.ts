10d220afde1bab36c5c27923b26a86a2b29387e8
myst <johndaveaquino35@gmail.com>
2025-01-18 15:40:41 +0800

    partial code for network route

---
+++ app/api/v1/network/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+interface NetworkData {
+    [operator: string]: string[];
+}
+
+interface CountryData {
+    name: string;
+    code: string;
+    continent: string;
+    region: string;
+    NETWORK: NetworkData;
+}
+
+export async function GET(req: NextRequest) {
+    const dataUrl = process.env.DATA_JSON_URL;
+
+    if (!dataUrl) {
+        return NextResponse.json(
+            { message: "Environment variable DATA_JSON_URL not set", error: true },
+            { status: 500 }
+        );
+    }
+
+    try {
+        // Fetch data from the URL
+        const response = await fetch(dataUrl);
+
+        if (!response.ok) {
+            throw new Error("Error fetching data");
+        }
+
+        // Parse the JSON data
+        const data:  CountryData  = await response.json();
+
+        if (data != null) {
+            return NextResponse.json(
+                { message: "Data fetched successfully", status: 200, data },
+                { status: 200 }
+            );
+        }
+    } catch (e) {
+        return NextResponse.json(
+            { message: "Error processing data", status: 500 },
+            { status: 500 }
+        );
+    }
+}
